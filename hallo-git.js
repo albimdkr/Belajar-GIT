107d46d3d11393c4d50a3fdfd85f4a7ce92b7bb2
Albi Mudakar <83914906+albimdkr@users.noreply.github.com>
2023-08-13 21:44:33 +0700

    add function

--- hallo-git.js
+++ hallo-git.js
@@ -1,10 +1,7 @@
 const word = "Hallo GIT from resolve conflict";
 console.log(word);
 
-const name = "hello git";
-console.log(name);
-
-const course = "bangkit"
-console.log(course);
+function helloFromBangkit(){
+}
 
 
